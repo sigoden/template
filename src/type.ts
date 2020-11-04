57488a23f016a2faa5f578242a94a711bac0a3fb
sigoden <sigoden@gmail.com>
2020-11-04 11:21:52 +0800

    feat: koa state type

--- src/type.ts
+++ src/type.ts
@@ -1,6 +1,11 @@
 import * as Koa from "koa";
 import * as api from "@/typeApi";
 
-export type Handler<T> = (req: T, ctx: Koa.Context) => Promise<any>;
+export type Handler<T> = (req: T, ctx: Koa.ParameterizedContext<KoaContextState>) => Promise<any>;
+export interface KoaContextState extends Koa.DefaultState {
+  auth?: {
+    name: string;
+  }
+}
 
 export { api };
