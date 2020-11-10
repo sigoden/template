0df522693f592c7a4fcf356b5462ea032ef62162
sigoden <sigoden@gmail.com>
2020-11-10 14:55:04 +0800

    update list-routes

--- scripts/list-routes.js
+++ scripts/list-routes.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const { parseOpenApi } = require("jsona-openapi-js");
 const [jsonaFile, prefix, format] = process.argv.slice(2);
 if (!jsonaFile || !prefix || !format) {
-  console.log("Usage: node ./list-routes.js <jsona-file> <prefix> <json|simple> ");
+  console.log("Usage: node ./list-routes.js <jsona-file> <prefix> <simple|json|htte> ");
   process.exit();
 }
 const METHODS = ["get", "put", "delete", "post", "options"];
@@ -10,16 +10,23 @@ const content = fs.readFileSync(jsonaFile, "utf8");
 const openapi = parseOpenApi(content);
 const operations = getOperations(openapi);
 
-if (format === "simple") {
-  operations.forEach(o => {
-    console.log(`${o.operationId}: "${o.method.toUpperCase()} ${withPrefix(o.path)}",`);
-  });
-} else {
+if (format === "json") {
   const result = operations.reduce((acc, cur) => {
     Object.assign(acc, { [cur.operationId]: { method: cur.method, path: withPrefix(cur.path) } });
     return acc;
   }, {});
   console.log(JSON.stringify(result, null, 2));
+} else if (format === "htte") {
+  operations.forEach(o => {
+    console.log(`  ${o.operationId}:
+    req:
+      method: ${o.method}
+      url: ${withPrefix(o.path)}`);
+  });
+} else {
+  operations.forEach(o => {
+    console.log(`${o.operationId}: "${o.method.toUpperCase()} ${withPrefix(o.path)}",`);
+  });
 }
 
 function withPrefix(path) {
