8913c5107f882d35419dcbbd7c7ad1294ff10667
sigoden <sigoden@gmail.com>
2020-12-20 15:51:42 +0800

    feat: ignore json error

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -22,7 +22,13 @@ export default function error() {
         ctx.body = err.toJSON();
         return;
       }
-      logger.error(err, collectHttpInfo(ctx));
+      if (err.name === "SyntaxError" && /Unexpected token/.test(err.message)) {
+        ctx.status = 400;
+        ctx.body = errs.ErrInternal.toJson({ message: "参数错误" });
+        return;
+      } else {
+        logger.error(err, collectHttpInfo(ctx));
+      }
       ctx.status = 500;
       ctx.body = errs.ErrInternal.toJson({ message: "server error" });
     }
