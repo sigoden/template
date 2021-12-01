683c05c3417307386e7f1939c43c3bcc9445d18f
sigoden <sigoden@gmail.com>
2021-12-01 20:25:18 +0800

    update handlers

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,10 +1,41 @@
-import { Post } from "@/models";
+import { User, Post } from "@/models";
 import srvs from "@/services";
-import { withPagination } from "@/lib/utils";
+import { passHash, passVerify, withPagination } from "@/lib/utils";
 import { kisa, okBody } from "@/app";
 
 export default function register() {
   const { errs } = srvs;
+
+  kisa.handlers.signup = async (ctx) => {
+    const { name, pass } = ctx.kisa.body;
+    let user = await User.findOne({ where: { name } });
+    if (user) {
+      throw errs.ErrUserName.toError();
+    }
+    user = await User.create({
+      name,
+      pass: await passHash(pass),
+    });
+
+    user = await user.reload();
+    ctx.body = await user.withAuthToken();
+  };
+
+  kisa.handlers.login = async (ctx) => {
+    const { name, pass } = ctx.kisa.body;
+    const user = await User.findOne({ where: { name } });
+    if (!user) {
+      throw errs.ErrNoUser.toError();
+    }
+    if (user.isForbid) {
+      throw errs.ErrForbidUser.toError();
+    }
+    const ok = await passVerify(pass, user.pass);
+    if (!ok) {
+      throw errs.ErrCheckPass.toError();
+    }
+    ctx.body = await user.withAuthToken();
+  };
 
   kisa.handlers.listPosts = async (ctx) => {
     const records = await Post.findAll(
--- src/handlers/index.ts
+++
@@ -1,7 +0,0 @@
-import registerAuth from "@/handlers/auth";
-import registerPost from "@/handlers/post";
-
-export default function register() {
-  registerAuth();
-  registerPost();
-}
